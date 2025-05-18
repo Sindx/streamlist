f5e6c721197dd683fbac7e9dc7309b3952cc9cd5
Sindx <seproth123@aol.com>
2025-05-18 12:24:41 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faFilm, faCartPlus, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>StreamList</h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome} /> Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/movies">
+            <FontAwesomeIcon icon={faFilm} /> Movies
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <FontAwesomeIcon icon={faCartPlus} /> Cart
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <FontAwesomeIcon icon={faInfoCircle} /> About
+          </Link>
+        </li>
       </ul>
     </nav>
   );
